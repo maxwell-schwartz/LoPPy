737a0037f1b5a640b87d40c6407d2d4e0990c575
Maxwell Schwartz <maxwell.schwartz11@gmail.com>
2017-10-26 01:02:56 -0400

    Import test cleaning

--- import_test.py
+++ import_test.py
@@ -1,23 +1,23 @@
 # Testing the functionality
 
-import loppy
+import loppy as lp
 
 def main():
     # I may have written this while very tired...
 
-    knowledge = loppy.FactTracker()
-    knowledge.update_knowledge(loppy.Fact('is tired', 'Max'))
-    knowledge.update_knowledge(loppy.Fact('ready for bed', 'Max'))
-    knowledge.update_knowledge(loppy.Fact('ready for bed', 'Most People'))
+    knowledge = lp.FactTracker()
+    knowledge.update_knowledge(lp.Fact('is tired', 'Max'))
+    knowledge.update_knowledge(lp.Fact('ready for bed', 'Max'))
+    knowledge.update_knowledge(lp.Fact('ready for bed', 'Most People'))
 
     print(knowledge.get_types('Max'))
 
-    # tired_and_ready = knowledge.solve_full(loppy.Fact('is tired', '?who'), 
-    #                     loppy.Fact('ready for bed', '?who'))
+    # tired_and_ready = knowledge.solve_full(lp.Fact('is tired', '?who'), 
+    #                     lp.Fact('ready for bed', '?who'))
     # for t in tired_and_ready:
     #     print('{} = tired and ready for bed.'.format(t[0][1]))
 
-    # ready = knowledge.solve_one(loppy.Fact('ready for bed', '?X'))
+    # ready = knowledge.solve_one(lp.Fact('ready for bed', '?X'))
     # for r in ready:
     #     print('{} = ready for bed.'.format(r[0][1]))
 
