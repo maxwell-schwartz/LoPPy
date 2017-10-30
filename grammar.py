633d6b3539ce9eb06c38711315c1dc503931fe92
Maxwell Schwartz <mschwartz@ets.org>
2017-10-30 14:49:21 -0400

    Additional functions added to grammar, word lists added for testing

--- grammar.py
+++ grammar.py
@@ -13,6 +13,17 @@ def is_np(knowledge, elements):
         return truth, ['ADJ'] + pos_list
     return False, []
 
+def is_dp(knowledge, elements):
+    '''Determine if a given set of words in a Determiner Phrase'''
+
+    if len(elements) < 2:
+        return False, []
+    head, *tail = elements
+    if knowledge.is_a([head], 'DET'):
+        truth, pos_list = is_np(knowledge, tail)
+        return truth, ['DET'] + pos_list
+    return False, []
+
 def is_advp(knowledge, elements):
     '''Determing if given set of words is a series of Adverbs'''
 
@@ -26,19 +37,59 @@ def is_advp(knowledge, elements):
         return truth, ['ADV'] + pos_list
     return False, []
 
+def is_dp_with_advp(knowledge, elements):
+    '''
+    Determine if given set of words is a Noun Phrase followed by a series of Adverbs
+    This is a potential component of Transitive Verb Phrases
+    '''
+    if len(elements) < 2:
+        return False, []
+    head, *tail = elements
+    head = [head]
+    while tail:
+        dp_truth, dp_pos = is_dp(knowledge, head)
+        advp_truth, advp_pos = is_advp(knowledge, tail)
+        if dp_truth and advp_truth:
+            return True, dp_pos + advp_pos
+        new_head, *tail = tail
+        head += [new_head]
+    return False, []
+
 def is_int_vp(knowledge, elements):
-    '''Determine if a given set of words is a Verb Phrase'''
+    '''Determine if a given set of words is an Intransitive Verb Phrase'''
 
     if len(elements) == 0:
         return False, []
     elif len(elements) == 1 and knowledge.is_a(elements, 'INT_VERB'):
+        # A lone verb works
         return True, ['INT_VERB']
     head, *tail = elements
     if knowledge.is_a([head], 'INT_VERB'):
+        # A verb followed by any number of adverbs works
         truth, pos_list = is_advp(knowledge, tail)
         return truth, ['INT_VERB'] + pos_list
     elif knowledge.is_a([head], 'ADV'):
+        # Any number of adverbs can precede the verb
         truth, pos_list = is_int_vp(knowledge, tail)
+        return truth, ['ADV'] + pos_list
+    return False, []
+
+def is_tr_vp(knowledge, elements):
+    '''Determine if given set of words is a Transitive Verb Phrase'''
+    if len(elements) < 2:
+        return False, []
+    head, *tail = elements
+    if knowledge.is_a([head], 'TR_VERB'):
+        # Check if words following the transitive verb are an NP or an NP+ADVP
+        dp_truth, dp_pos_list = is_dp(knowledge, tail)
+        dp_advp_truth, dp_advp_pos_list = is_dp_with_advp(knowledge, tail)
+        if dp_truth:
+            return True, ['TR_VERB'] + dp_pos_list
+        elif dp_advp_truth:
+            return True, ['TR_VERB'] + dp_advp_pos_list
+    elif knowledge.is_a([head], 'ADV'):
+        # Any number of adverbs can precede the verb
+        truth, pos_list = is_tr_vp(knowledge, tail)
         return truth, ['ADV'] + pos_list
     return False, []
 
@@ -51,10 +102,14 @@ def is_sentence(knowledge, elements):
         head, *tail = elements
         head = [head]
         while tail:
-            subj_truth, subj_pos = is_np(knowledge, head)
-            pred_truth, pred_pos = is_int_vp(knowledge, tail)
-            if subj_truth and pred_truth:
-                return True, subj_pos + pred_pos
+            subj_truth, subj_pos = is_dp(knowledge, head)
+            tr_truth, tr_pos = is_tr_vp(knowledge, tail)
+            int_truth, int_pos = is_int_vp(knowledge, tail)
+            if subj_truth:
+                if tr_truth:
+                    return True, subj_pos + tr_pos
+                elif int_truth:
+                    return True, subj_pos + int_truth
             new_head, *tail = tail
             head += [new_head] 
     return False, []
@@ -62,27 +117,43 @@ def is_sentence(knowledge, elements):
 def main():
     # Test
     knowledge = lp.FactTracker()
-    knowledge.update_knowledge(lp.Fact('NOUN', 'dog'))
-    knowledge.update_knowledge(lp.Fact('NOUN', 'cat'))
-    knowledge.update_knowledge(lp.Fact('ADJ', 'small'))
-    knowledge.update_knowledge(lp.Fact('ADJ', 'blue'))
-    knowledge.update_knowledge(lp.Fact('INT_VERB', 'dances'))
-    knowledge.update_knowledge(lp.Fact('ADV', 'quickly'))
-    knowledge.update_knowledge(lp.Fact('ADV', 'often'))
+    with open('word_lists/nouns.txt', 'r') as infile:
+        nouns = infile.readlines()
+    with open('word_lists/transitive_verbs.txt', 'r') as infile:
+        tr_verbs = infile.readlines()
+    with open('word_lists/intransitive_verbs.txt', 'r') as infile:
+        int_verbs = infile.readlines()
+    with open('word_lists/adjectives.txt', 'r') as infile:
+        adjectives = infile.readlines()
+    with open('word_lists/adverbs.txt', 'r') as infile:
+        adverbs = infile.readlines()
+    with open('word_lists/determiners.txt', 'r') as infile:
+        determiners = infile.readlines()
+
+    for n in nouns:
+        knowledge.update_knowledge(lp.Fact('NOUN', n.strip()))
+    for t in tr_verbs:
+        knowledge.update_knowledge(lp.Fact('TR_VERB', t.strip()))
+    for i in int_verbs:
+        knowledge.update_knowledge(lp.Fact('INT_VERB', i.strip()))
+    for a in adjectives:
+        knowledge.update_knowledge(lp.Fact('ADJ', a.strip()))
+    for ad in adverbs:
+        knowledge.update_knowledge(lp.Fact('ADV', ad.strip()))
+    for d in determiners:
+        knowledge.update_knowledge(lp.Fact('DET', d.strip()))
 
     keep_going = 'y'
     while keep_going == 'y':
         user_sent = input('Enter sentence >> ').split()
         # print('NP > ', is_np(knowledge, user_sent))
         # print('INT_VP > ', is_int_vp(knowledge, user_sent))
+        # print('TR_VP > ', is_tr_vp(knowledge, user_sent))
         # print('ADVP > ', is_advp(knowledge, user_sent))
+        # print('DP + ADVP > ', is_dp_with_advp(knowledge, user_sent))
+        # print('DP > ', is_dp(knowledge, user_sent))
         print('SENTENCE > ', is_sentence(knowledge, user_sent))
         keep_going = input('Continue? (y/n) ')
-
-    # print(is_np(knowledge, 'small dog'.split()))
-    # print(is_np(knowledge, 'small blue small dog'.split()))
-    # print(is_np(knowledge, 'cat'.split()))
-    # print(is_np(knowledge, 'small blue'.split()))
 
 if __name__ == '__main__':
     main()
