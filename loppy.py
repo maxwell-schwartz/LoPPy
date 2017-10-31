200806444330fe5f0ae7ca5ebdcf0293d2e1b298
Maxwell Schwartz <mschwartz@ets.org>
2017-10-31 15:51:08 -0400

    Solve method output cleaned, examples expanded

--- loppy.py
+++ loppy.py
@@ -65,7 +65,7 @@ class FactTracker(object):
             # If choice is viable at the end, provide all solutions to unknowns
             if viable:
                 solution = [(fact.elements[u], choice[u]) for u in unknowns]
-                yield tuple(solution)
+                yield '{} = {}'.format(solution[0][0], solution[0][1])
 
     def solve_full(self, *args):
         '''Yield only solutions that satisfy all searched facts'''
@@ -89,22 +89,3 @@ class Fact(object):
     def get_args(self):
         print(self.name)
         print(self.elements)
-
-def main():
-    # For testing purposes
-    # Yes, these are my real family members
-    tracker = FactTracker()
-    tracker.update_knowledge(Fact('child', 'Max', 'Jan', 'Ellen'))
-    tracker.update_knowledge(Fact('child', 'Dash', 'Jan', 'Ellen'))
-    tracker.update_knowledge(Fact('child', 'Rocky', 'Jan', 'Ellen'))
-    tracker.update_knowledge(Fact('spouse', 'Jan', 'Ellen'))
-    tracker.update_knowledge(Fact('brother', 'Max', 'Dash'))
-    tracker.update_knowledge(Fact('brother', 'Max', 'Rocky'))
-    tracker.update_knowledge(Fact('brother', 'Dash', 'Rocky'))
-    
-    for i in (tracker.solve_full(Fact('child', '?X', 'Jan', 'Ellen'), 
-                Fact('brother', '?X', 'Rocky'))):
-        print(i)
-
-if __name__ == "__main__":
-    main()
